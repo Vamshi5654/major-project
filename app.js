8d95a6a3d8dab113200b4c17265b7924692b936a
Delta Student <bandarivamshi07@gmail.com>
2025-07-10 12:16:17 +0530

    fix: handle currUser and middleware placement

--- app.js
+++ app.js
@@ -93,6 +93,7 @@ app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   res.locals.currUser = req.user;
+  console.log("Current user:", req.user); 
   next();
 });
 
