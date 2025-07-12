b236653b2834ecf165589f125afc3d11a754c3f4
Delta Student <bandarivamshi07@gmail.com>
2025-07-12 07:48:21 +0530

    Fix show route and ensure listing links work

--- controllers/listings.js
+++ controllers/listings.js
@@ -13,17 +13,28 @@ module.exports.renderNewForm =  (req ,res) => {
 
 };
 
+// module.exports.showListing = async(req,res) => {
+//     let {id} = req.params;
+//      const listing = await Listing.findById(id).populate({path : "reviews",populate : { path : "author"},}).populate("owner");
+//      if(!listing){
+//         req.flash("error","Listing you requested for does not exist!");
+//         res.redirect("/listings");
+//      }
+     
+//     res.render("listings/show.ejs",{listing});
+
+// };
+
 module.exports.showListing = async(req,res) => {
     let {id} = req.params;
-     const listing = await Listing.findById(id).populate({path : "reviews",populate : { path : "author"},}).populate("owner");
-     if(!listing){
-        req.flash("error","Listing you requested for does not exist!");
-        res.redirect("/listings");
-     }
-     
-    res.render("listings/show.ejs",{listing});
-
+    const listing = await Listing.findById(id).populate({ path: "reviews", populate: { path: "author" } }).populate("owner");
+    if (!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        return res.redirect("/listings");
+    }
+    res.render("listings/show.ejs", { listing });
 };
+
 
 // module.exports.createListing = async (req,res,next) => {
 
